4cc502e8011bae52c7d0ed377b812a190de24eed
Paolo Capriotti <paolo@capriotti.io>
2019-09-13 01:21:37 +0200

    Set up task descriptor in stage 1

--- src/stage1.c
+++ src/stage1.c
@@ -4,18 +4,42 @@ extern volatile uint8_t *vesa_framebuffer;
 extern unsigned short vesa_pitch;
 
 typedef struct {
+  uint32_t prev;
+  uint32_t esp0, ss0;
+  uint32_t esp1, ss1;
+  uint32_t esp2, ss2;
+  uint32_t cr3, eip, eflags;
+  uint32_t eax, ecx, edx, ebx;
+  uint32_t esp, ebp, esi, edi;
+  uint32_t es, cs, ss, ds, fs, gs;
+  uint32_t ldt, trap, iomap_base;
+} __attribute__((packed)) tss_t;
+
+static tss_t kernel_tss;
+
+typedef struct {
   uint16_t limit_low;
   uint16_t base_low;
   uint8_t base_mid;
-  uint8_t access;
+  uint8_t flags;
   uint8_t granularity;
   uint8_t base_high;
 } __attribute__((packed)) gdt_entry_t;
 
+enum {
+  GDT_NULL = 0,
+  GDT_CODE,
+  GDT_DATA,
+  GDT_TASK,
+
+  GDT_NUM_ENTRIES,
+};
+
 static __attribute__((aligned(8))) gdt_entry_t kernel_gdt[] = {
   { 0, 0, 0, 0, 0, 0 },
   { 0xffff, 0, 0, 0x9a, 0xcf, 0 }, /* code segment */
-  { 0xffff, 0, 0, 0x92, 0xcf, 0 }, /* code segment */
+  { 0xffff, 0, 0, 0x92, 0xcf, 0 }, /* data segment */
+  { 0, 0, 0, 0, 0, 0 }, /* placeholder for task descriptor */
 };
 
 typedef struct {
@@ -28,14 +52,33 @@ gdtp_t kernel_gdtp = {
   kernel_gdt
 };
 
+void set_gdt_entry(gdt_entry_t *entry,
+                   uint32_t base,
+                   uint32_t limit,
+                   uint8_t flags,
+                   uint8_t granularity)
+{
+  entry->limit_low = limit & 0xffff;
+  entry->granularity = ((granularity << 4) & 0xf0) |
+    ((limit >> 16) & 0x0f);
+  entry->base_low = base & 0xffff;
+  entry->base_mid = ((base >> 16) & 0xff);
+  entry->base_high = ((base >> 24) & 0xff);
+  entry->flags = flags;
+}
+
 void _stage1()
 {
+  set_gdt_entry(&kernel_gdt[GDT_TASK],
+                (uint32_t)&kernel_tss,
+                sizeof(kernel_tss),
+                0x89, 0);
+
   for (int j = 0; j < 10; j++) {
     for (int i = 0; i < 10; i++) {
-      vesa_framebuffer[i + j * vesa_pitch] = 4;
+      vesa_framebuffer[i + j * vesa_pitch] = 2;
     }
   }
-
   __asm__ volatile("hlt");
   while (1);
 }
