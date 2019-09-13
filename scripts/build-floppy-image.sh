10f7ce557c0690770481644afd628895b4e7d0ca
Paolo Capriotti <paolo@capriotti.io>
2019-09-13 23:03:23 +0200

    Build floppy image with a script

---
+++ scripts/build-floppy-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+die () {
+    echo "$1" >&2
+    exit 1
+}
+
+[ "$#" == 2 ] || die "Usage: $0 INPUT OUTPUT"
+
+sz=$(stat --printf="%s" "$1")
+sectors=$(((sz + 511) / 512))
+echo "$sectors sectors"
+
+[ "$sectors" -le 18 ] || die "Binary too large ($sectors sectors)"
+
+osz=$((sectors * 512))
+dd if="$1" of="$2" bs=512
+dd if=/dev/null of="$2" bs=1 count=1 seek="$osz"
