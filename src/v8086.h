2cd8eaf3ea23726a425aed0978e14bf0469b9d22
Paolo Capriotti <paolo@capriotti.io>
2019-09-14 17:35:53 +0200

    Fix v8086 segment registers

--- src/v8086.h
+++ src/v8086.h
@@ -2,11 +2,12 @@
 #define V8086_H
 
 #define V8086_STACK_BASE 0x2000
+#define V8086_HEAP 0x2800
 
 #if !__ASSEMBLER__
 typedef struct {
   uint16_t ax, bx, cx, dx;
-  uint16_t gs, fs, es, ds;
+  uint16_t es, ds, fs, gs;
 } regs16_t;
 
 void v8086_enter(uint32_t interrupt, regs16_t *regs);
