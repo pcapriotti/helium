6a0114a538ff1371aaaab45b54a9bff81881fed9
Paolo Capriotti <paolo@capriotti.io>
2019-09-21 23:32:23 +0200

    Add script to generate disk image

---
+++ scripts/make-image.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+parse_size() {
+    if [[ "$1" == *s ]]
+    then
+        echo $((${1%s} * 512))
+    elif [[ "$1" == *k ]]
+    then
+        echo $((${1%k} * 1024))
+    elif [[ "$1" == *M ]]
+    then
+        echo $((${1%M} * 1024 * 1024))
+    elif [[ "$1" == *G ]]
+    then
+        echo $((${1%G} * 1024 * 1024 * 1024))
+    else
+        echo "$1"
+    fi
+}
+
+usage() {
+    echo "Usage: $0 [-s SIZE] [-o OFFSET] DISK DIR" >&2
+    exit 1
+}
+
+while getopts "s:o:" x; do
+    case $x in
+        s) size=$(parse_size "$OPTARG");;
+        o) offset=$(parse_size "$OPTARG");;
+    esac
+done
+shift $((OPTIND - 1))
+disk=$1
+src=$2
+[ -n "$disk" ] || usage
+[ -n "$src" ] || usage
+[ -z "$3" ] || usage
+
+: ${size:=$((10 * 1024 * 1024))}
+: ${offset:=$((72 * 512))}
+
+dd if=/dev/zero of="$disk" bs=1024 count=$(((size + 1023) / 1024)) >&2
+parted -m -s "$disk" -- mklabel msdos >&2
+parted -m -s "$disk" -- mkpart primary ext2 "${offset}B" "-1s" >&2
+mke2fs -d image -t ext2 -q -F -F -E offset="$offset",no_copy_xattrs "$disk" "$(((size - offset) / 1024))" >&2
