fe238db5df62378bb708d526ae35038b138ac2c3
Paolo Capriotti <paolo@capriotti.io>
2019-10-19 16:01:07 +0100

    Use serial_printf for assertions

--- libc/assert.h
+++ libc/assert.h
@@ -2,7 +2,7 @@
 #define ASSERT_H
 
 #define assert(e) do { \
-  if (!(e)) { kprintf("assert: %s\n", #e); \
+  if (!(e)) { serial_printf("assert: %s\n", #e); \
     _panic(__FILE__, __LINE__); } } while(0)
 
 #endif /* ASSERT_H */
