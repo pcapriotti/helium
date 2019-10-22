854b22b5a90754bc93139da1aa49744812deb62d
Paolo Capriotti <paolo@capriotti.io>
2019-10-22 10:54:59 +0100

    Add some rect functions

--- kernel/console/rect.c
+++ kernel/console/rect.c
@@ -1,1 +1,47 @@
 #include "rect.h"
+
+void rect_expand(rect_t *rect, point_t p)
+{
+  if (rect->height == 0 || rect->width == 0) {
+    rect->x = p.x;
+    rect->y = p.y;
+  }
+
+  if (p.x < rect->x) {
+    rect->x = p.x;
+  }
+  else if (p.x >= rect->x + rect->width) {
+    rect->width = p.x - rect->x + 1;
+  }
+
+  if (p.y < rect->y) {
+    rect->y = p.y;
+  }
+  else if (p.y >= rect->y + rect->height) {
+    rect->height = p.y - rect->y + 1;
+  }
+}
+
+void rect_bounding(rect_t *rect1, rect_t *rect2)
+{
+  if (rect1->height == 0 || rect1->width == 0) {
+    rect1->x = rect2->x;
+    rect1->y = rect2->y;
+  }
+
+  if (rect2->x < rect1->x) {
+    rect1->width += rect1->x - rect2->x;
+    rect1->x = rect2->x;
+  }
+  if (rect2->x + rect2->width > rect1->x + rect1->width) {
+    rect1->width = rect2->x + rect2->width - rect1->x;
+  }
+
+  if (rect2->y < rect1->y) {
+    rect1->height += rect1->y - rect2->y;
+    rect1->y = rect2->y;
+  }
+  if (rect2->x + rect2->height > rect1->x + rect1->height) {
+    rect1->height = rect2->y + rect2->height - rect1->y;
+  }
+}
--- kernel/console/rect.h
+++ kernel/console/rect.h
@@ -1,8 +1,15 @@
 #ifndef CONSOLE_RECT_H
 #define CONSOLE_RECT_H
 
+#include "console/point.h"
+
 typedef struct rect {
   int x, y, width, height;
 } rect_t;
+
+void rect_expand(rect_t *rect, point_t p);
+
+/* extend the first rect so that it contains the second one */
+void rect_bounding(rect_t *rect1, rect_t *rect2);
 
 #endif /* CONSOLE_RECT_H */
