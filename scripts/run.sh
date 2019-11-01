e65562de1d00bbe6bb5294c004709fd6b14f842c
Paolo Capriotti <paolo@capriotti.io>
2019-11-01 22:14:33 +0100

    Improve run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
-: ${MEM:=2G}
 tup
-qemu-system-i386 -hda build/disk.img -serial stdio -m $MEM
+
+: ${MEM:=2G}
+: ${QEMUSYS:=i386}
+: ${QEMU:=qemu-system-$QEMUSYS}
+: ${SERIAL:=stdio}
+
+if [[ -n "$AHCI" ]]; then
+    opt_ahci="-device ahci,id=ahci"
+    opt_bus=",bus=ahci.0"
+fi
+
+$QEMU -serial $SERIAL \
+      -m $MEM \
+      -drive id=disk,file=build/disk.img,if=none \
+      $opt_ahci \
+      -device "ide-drive,drive=disk$opt_bus"
